690c97e7d5675f7dda60f0d784ad17ea7f8a8b1e
main <375277023@qq.com>
2019-07-31 14:47:55 +0800

    完善resume.js

--- pages/resume/js/resume.js
+++ pages/resume/js/resume.js
@@ -9,7 +9,7 @@ var resume = {
       'picture': '/static/images/avatar.jpg',
       'email': '375277023@qq.com',
       'phone': '13322061910',
-      'website': 'https://github.com/skyvow',
+      'website': 'https://github.com/main2018',
       'summary': [
           '3年+前端开发经验，参与或独立开发过多个核心项目，项目类型涉及：电子商务、房地产、企业管理、SPA 应用（PC/Moblie）等等',
           '喜欢关注 Web 前端相关新技术（Vue, Flutter...），对于新技术有很强的学习兴趣',
@@ -55,201 +55,186 @@ var resume = {
               '负责前端技术的完善，以用户为中心，不断尝试前端新技术，提升团队水平'
           ]
       },
-      {
-          'company': '上海连趣跑信息技术有限公司',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2015.10',
-          'endDate': '2016.04',
-          'summary': '上海连趣跑信息技术有限公司',
-          'highlights': [
-              'Photoshop 对设计图进行切片',
-              '根据设计提供的设计稿编写静态页面',
-              '负责配合后台开发人员，完成页面的交互及接口对接',
-              '负责网站功能模块及后台管理系统的设计'
-          ]
-      },
-      {
-          'company': '上海艺术伙伴有限公司',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2015.05',
-          'endDate': '2015.10',
-          'summary': '上海艺术伙伴有限公司',
-          'highlights': [
-              'Photoshop 对设计图进行切片',
-              'HTML5 + CSS3 编写网站页面，并保证浏览器的兼容性',
-              '负责配合后台开发人员，完成页面的交互及接口对接',
-              '负责电脑端及移动端 Web 项目的前端开发'
-          ]
-      }
+      // {
+      //     'company': '上海连趣跑信息技术有限公司',
+      //     'position': 'Web 前端工程师',
+      //     'website': '',
+      //     'startDate': '2015.10',
+      //     'endDate': '2016.04',
+      //     'summary': '上海连趣跑信息技术有限公司',
+      //     'highlights': [
+      //         'Photoshop 对设计图进行切片',
+      //         '根据设计提供的设计稿编写静态页面',
+      //         '负责配合后台开发人员，完成页面的交互及接口对接',
+      //         '负责网站功能模块及后台管理系统的设计'
+      //     ]
+      // },
   ],
   'project': [
-      {
-          'organization': '美味&招行项目对接',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2018.12',
-          'endDate': '2019.01',
-          'summary': 'H5 开发',
-          'highlights': [
-              '美味&招行信用卡项目对接；',
-              '实现排队、预订、秒点、秒付等功能；',
-              '实现招行信用卡中心内扫码订单，支付等功能。'
-          ]
-      },
-      {
-          'organization': '友车友家小程序开发',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2018.10',
-          'endDate': '2018.12',
-          'summary': '微信小程序',
-          'highlights': [
-              '友车友家微信小程序开发，主要为新车、二手车平台；',
-              '包括选择车辆预约门店，预约看车，在线咨询等功能；',
-              '我在此项目中担任 Web 前端工程师，负责参与项目的前端开发及维护；',
-              '实现了选车、特惠、门店、我的等主要功能；',
-              '实现了海报分享、抽奖转盘等推销功能。'
-          ]
-      },
-      {
-          'organization': '供应链 SaaS 软件',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2017.06',
-          'endDate': '2018.12',
-          'summary': 'React + React-router + Redux + Wepack + ES6',
-          'highlights': [
-              '我就职于美味供应链事业部，开发团队一共十多人，协同开发一款供应链 SaaS 软件，主要功能包括门店管理、集采中心管理等。',
-              '我在此项目中担任 Web 前端工程师，负责参与项目的前端开发及维护，开发完成后，到自测阶段，再到发布正式环境。',
-              '项目基于 React + React-router + Redux + Wepack + ES6 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码，热更新；由于旧版本线上编译发布时间较长（约20分钟），通过 Wepack 开启多线程、按需加载等功能优化使之编译速度到达约 12 分钟。',
-              '项目正式上线后，Web 前端性能及用户体验表现良好，为商家提供了便利。'
-          ]
-      },
-      {
-          'organization': '美味供应链（微信小程序）',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2018.05',
-          'endDate': '2018.06',
-          'summary': '微信小程序',
-          'highlights': [
-              '2018年5月到2018年6月负责开发的一款美味供应链（微信小程序）的项目，主要功能包括下单，消息通知、查看报表等功能。',
-              '由于是新需求新项目，只有一个人参与前端开发，前期调研了小程序与公众号通信等相关资料，以及参与项目的需求评审，到确定设计稿阶段。',
-              '根据 UI 提供的设计稿，百分百还原页面及其交互效果，并且通过小程序 rpx 设计方案兼容多尺寸移动设备。',
-              '项目经验收，无重大漏洞，正式上线后，用户体验良好，功能完善。'
-          ]
-      },
-      {
-          'organization': '颜值评分',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2017.04',
-          'endDate': '2017.05',
-          'summary': '微信小程序',
-          'highlights': [
-              '2017年4月到2017年5月独立负责开发的一款颜值评分项目，包括颜值 PK、夫妻相、颜值榜等功能；',
-              '由于是个人项目，一个人担任多项角色负责参与产品需求、页面交互设计过程、前后端的开发、Ubuntu 16.04 服务器部署等工作；',
-              '前端采用微信小程序开发，在此封装了高效的可复用的组件模块，通过组件化开发极大的提升了开发效率；后端采用 NodeJS 开发，基于 Express 框架提供 RESTful API；',
-              '项目中采用 Face++ 人脸识别服务技术，检测并定位图片中的人脸，返回人脸坐标，然后通过面部黄金比例计算出颜值评分；',
-              '项目上线后用户体验流畅，用户数量不断增加，至此已达 5 万多。'
-          ]
-      },
-      {
-          'organization': '上海中科院后台管理',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2017.01',
-          'endDate': '2017.04',
-          'summary': 'React + React-router + Redux + Wepack + ES6 + PostCSS',
-          'highlights': [
-              '2017年1月到2017年4月我们四人小组负责开发的一款 CMS 内容管理系统项目包括内容管理、模板管理、发布管理，为上海中科院提供可视化后台管理功能；',
-              '作为团队中的核心程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
-              '项目基于 React + React-router + Redux + Wepack + ES6 + PostCSS 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码，热更新；',
-              '项目上线后，前端性能表现良好，测试结果也没有任何异常，客户也很满意。'
-          ]
-      },
-      {
-          'organization': '上海高校后勤e站',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2016.10',
-          'endDate': '2016.12',
-          'summary': 'AngularJS + Wepack + ES6 + PostCSS',
-          'highlights': [
-              '2016年10月到2016年12月我们四人小组负责开发一款会议管理系统的项目，包括会议预订、信息发布、微信签到、扫码签到等功能；',
-              '作为团队中的核心程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
-              '项目基于 AngularJS + Wepack + ES6 + PostCSS 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码；',
-              '项目上线后，已多次试点于中小型重要会议中，参会人员能随时轻松查看活动信息、下载资料，更为便捷。'
-          ]
-      },
-      {
-          'organization': '智慧微校园',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2016.04',
-          'endDate': '2016.10',
-          'summary': 'AngularJS + Wepack + ES6 + PostCSS',
-          'highlights': [
-              '2016年4月到2016年10月我们四人小组负责开发一款智慧微校园的项目，包括班级管理、成绩管理、信息发布，报修管理等功能，将教学、科研、管理和校园生活进行充分融合；',
-              '作为团队中的核心程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
-              '项目基于 AngularJS + Wepack + ES6 + PostCSS 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码；',
-              '项目正式在龚路小学上线之后，提升了学校日常管理效率，课堂效率和质量直线走高，受到教师与家长的广泛好评。'
-          ]
-      },
-      {
-          'organization': '比赛助手后台开发 前端重构',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2016.02',
-          'endDate': '2016.03',
-          'summary': 'AngularJS + Wepack + ES6 + PostCSS',
-          'highlights': [
-              '项目基于 AngularJS + Wepack + ES6 + PostCSS 重构项目，克服了多人协作开发的问题，使得研发成本大大降低、快速上线。'
-          ]
-      },
-      {
-          'organization': '比赛助手后台开发',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2015.12',
-          'endDate': '2016.02',
-          'summary': 'AngularJS + Wepack + ES6 + PostCSS',
-          'highlights': [
-              '比赛助手后台采用 AngularJS + Wepack + ES6 + PostCSS 为主体框架开发，主要完成登陆注册模块、比赛赛事创建功能及相关赛事的辅助模块：赛事管理、赛事查询、选手管理、成绩管理、证书编辑',
-              '赛事管理主要包括比赛赛事的创建至比赛结束后成绩的及时发布，以及赛后成绩查询',
-              '选手管理主要包括选手信息列表查询及导入、编辑、新增等操作'
-          ]
-      },
-      {
-          'organization': '比赛成绩查询开发',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2015.12',
-          'endDate': '2016.02',
-          'summary': 'AngularJS + Gulp + Sass',
-          'highlights': [
-              '前端基础框架搭建及简单界面设计，基于 AngularJS + Gulp + Sass 开发',
-              '比赛成绩查询功能模块的实现（赛事列表、赛事科目、团队成绩、个人成绩等）',
-              '根据特殊的比赛定制不同的查询页面'
-          ]
-      },
-      {
-          'organization': '跑团助手2.0开发',
-          'position': 'Web 前端工程师',
-          'website': '',
-          'startDate': '2015.12',
-          'endDate': '2016.02',
-          'summary': 'AngularJS + Gulp + Sass',
-          'highlights': [
-              '项目基于 AngularJS + Gulp + Sass 开发',
-              '跑团助手2.0开发，服务于跑友参加各种约跑活动提供便利',
-              '微信端授权确认身份后才登录',
-              '跑团团长可以发布活动、编辑活动、取消活动',
-              '跑友报名活动、交报名费、签到活动、分享活动'
-          ]
-      }
+    {
+      'organization': '新社区云服务平台',
+      'position': 'Web 前端工程师',
+      'website': 'http://www.xsqhomes.com/',
+      'startDate': '2016.8',
+      'endDate': '2017.4',
+      'summary': 'H5 开发',
+      'highlights': [
+        '物业管理系统',
+        '四大模块： 物业公司后台/业主微信端/系统管理平台/物业人员移动端',
+        '实现 在线小区管理， 业主缴费， 故障报修 等'
+      ]
+    },
+    {
+        'organization': '房公信',
+        'position': 'Web 前端工程师',
+        'website': 'https://www.fanggongxin.net/',
+        'startDate': '2017.6',
+        'endDate': '2017.10',
+        'summary': '微信公众号',
+        'highlights': [
+          '二手房在线交易平台',
+          '附近找房，地图搜索，在线交易',
+          '我在此项目中担任 Web 前端工程师，负责参与项目的前端开发及维护；',
+          '实现了 地图 在线聊天 支付等功能。'
+        ]
+    },
+    {
+        'organization': '徒客共享',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2017.06',
+        'endDate': '2018.12',
+        'summary': 'Vue Vue-Router Vuex Axios',
+        'highlights': [
+            '我就职于美味供应链事业部，开发团队一共十多人，协同开发一款供应链 SaaS 软件，主要功能包括门店管理、集采中心管理等。',
+            '我在此项目中担任 Web 前端工程师，负责参与项目的前端开发及维护，开发完成后，到自测阶段，再到发布正式环境。',
+            '项目基于 React + React-router + Redux + Wepack + ES6 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码，热更新；由于旧版本线上编译发布时间较长（约20分钟），通过 Wepack 开启多线程、按需加载等功能优化使之编译速度到达约 12 分钟。',
+            '项目正式上线后，Web 前端性能及用户体验表现良好，为商家提供了便利。'
+        ]
+    },
+    {
+        'organization': '美味供应链（微信小程序）',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2018.05',
+        'endDate': '2018.06',
+        'summary': '微信小程序',
+        'highlights': [
+            '2018年5月到2018年6月负责开发的一款美味供应链（微信小程序）的项目，主要功能包括下单，消息通知、查看报表等功能。',
+            '由于是新需求新项目，只有一个人参与前端开发，前期调研了小程序与公众号通信等相关资料，以及参与项目的需求评审，到确定设计稿阶段。',
+            '根据 UI 提供的设计稿，百分百还原页面及其交互效果，并且通过小程序 rpx 设计方案兼容多尺寸移动设备。',
+            '项目经验收，无重大漏洞，正式上线后，用户体验良好，功能完善。'
+        ]
+    },
+    {
+        'organization': '颜值评分',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2017.04',
+        'endDate': '2017.05',
+        'summary': '微信小程序',
+        'highlights': [
+            '2017年4月到2017年5月独立负责开发的一款颜值评分项目，包括颜值 PK、夫妻相、颜值榜等功能；',
+            '由于是个人项目，一个人担任多项角色负责参与产品需求、页面交互设计过程、前后端的开发、Ubuntu 16.04 服务器部署等工作；',
+            '前端采用微信小程序开发，在此封装了高效的可复用的组件模块，通过组件化开发极大的提升了开发效率；后端采用 NodeJS 开发，基于 Express 框架提供 RESTful API；',
+            '项目中采用 Face++ 人脸识别服务技术，检测并定位图片中的人脸，返回人脸坐标，然后通过面部黄金比例计算出颜值评分；',
+            '项目上线后用户体验流畅，用户数量不断增加，至此已达 5 万多。'
+        ]
+    },
+    {
+        'organization': '上海中科院后台管理',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2017.01',
+        'endDate': '2017.04',
+        'summary': 'React + React-router + Redux + Wepack + ES6 + PostCSS',
+        'highlights': [
+            '2017年1月到2017年4月我们四人小组负责开发的一款 CMS 内容管理系统项目包括内容管理、模板管理、发布管理，为上海中科院提供可视化后台管理功能；',
+            '作为团队中的核心程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
+            '项目基于 React + React-router + Redux + Wepack + ES6 + PostCSS 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码，热更新；',
+            '项目上线后，前端性能表现良好，测试结果也没有任何异常，客户也很满意。'
+        ]
+    },
+    {
+        'organization': '上海高校后勤e站',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2016.10',
+        'endDate': '2016.12',
+        'summary': 'AngularJS + Wepack + ES6 + PostCSS',
+        'highlights': [
+            '2016年10月到2016年12月我们四人小组负责开发一款会议管理系统的项目，包括会议预订、信息发布、微信签到、扫码签到等功能；',
+            '作为团队中的核心程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
+            '项目基于 AngularJS + Wepack + ES6 + PostCSS 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码；',
+            '项目上线后，已多次试点于中小型重要会议中，参会人员能随时轻松查看活动信息、下载资料，更为便捷。'
+        ]
+    },
+    {
+        'organization': '智慧微校园',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2016.04',
+        'endDate': '2016.10',
+        'summary': 'AngularJS + Wepack + ES6 + PostCSS',
+        'highlights': [
+            '2016年4月到2016年10月我们四人小组负责开发一款智慧微校园的项目，包括班级管理、成绩管理、信息发布，报修管理等功能，将教学、科研、管理和校园生活进行充分融合；',
+            '作为团队中的核心程序员，我负责项目的调研，根据产品需求文档进行需求拆分，技术选型与搭建框架等开发；',
+            '项目基于 AngularJS + Wepack + ES6 + PostCSS 前端框架配合 NodeJS 开发环境开发，采用 Webpack 作为自动化构建工具，实时编译代码；',
+            '项目正式在龚路小学上线之后，提升了学校日常管理效率，课堂效率和质量直线走高，受到教师与家长的广泛好评。'
+        ]
+    },
+    {
+        'organization': '比赛助手后台开发 前端重构',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2016.02',
+        'endDate': '2016.03',
+        'summary': 'AngularJS + Wepack + ES6 + PostCSS',
+        'highlights': [
+            '项目基于 AngularJS + Wepack + ES6 + PostCSS 重构项目，克服了多人协作开发的问题，使得研发成本大大降低、快速上线。'
+        ]
+    },
+    {
+        'organization': '比赛助手后台开发',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2015.12',
+        'endDate': '2016.02',
+        'summary': 'AngularJS + Wepack + ES6 + PostCSS',
+        'highlights': [
+            '比赛助手后台采用 AngularJS + Wepack + ES6 + PostCSS 为主体框架开发，主要完成登陆注册模块、比赛赛事创建功能及相关赛事的辅助模块：赛事管理、赛事查询、选手管理、成绩管理、证书编辑',
+            '赛事管理主要包括比赛赛事的创建至比赛结束后成绩的及时发布，以及赛后成绩查询',
+            '选手管理主要包括选手信息列表查询及导入、编辑、新增等操作'
+        ]
+    },
+    {
+        'organization': '比赛成绩查询开发',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2015.12',
+        'endDate': '2016.02',
+        'summary': 'AngularJS + Gulp + Sass',
+        'highlights': [
+            '前端基础框架搭建及简单界面设计，基于 AngularJS + Gulp + Sass 开发',
+            '比赛成绩查询功能模块的实现（赛事列表、赛事科目、团队成绩、个人成绩等）',
+            '根据特殊的比赛定制不同的查询页面'
+        ]
+    },
+    {
+        'organization': '跑团助手2.0开发',
+        'position': 'Web 前端工程师',
+        'website': '',
+        'startDate': '2015.12',
+        'endDate': '2016.02',
+        'summary': 'AngularJS + Gulp + Sass',
+        'highlights': [
+            '项目基于 AngularJS + Gulp + Sass 开发',
+            '跑团助手2.0开发，服务于跑友参加各种约跑活动提供便利',
+            '微信端授权确认身份后才登录',
+            '跑团团长可以发布活动、编辑活动、取消活动',
+            '跑友报名活动、交报名费、签到活动、分享活动'
+        ]
+    }
   ],
   'education': [
     {
